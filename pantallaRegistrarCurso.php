d1248ecb3f8c766a86a876248ba3f8c332d0b94d
azaelalanis <azaelalanis.g@gmail.com>
2014-11-30 18:37:48 -0600

    Cambios

--- pantallaRegistrarCurso.php
+++ pantallaRegistrarCurso.php
@@ -12,7 +12,7 @@ $FechaNacimiento=$row['FechaNacimiento'];
 $now = new DateTime();
 $fecha = new DateTime($FechaNacimiento);
 $diff = abs($now->format("Y") - $fecha->format("Y"));
-$sql="select 
+$sql="select
         idCurso,
         Nombre,
         Cupo,
@@ -49,7 +49,7 @@ $sql="select
       <a href="pantallaIndexStaff.php" class="navbar-brand">Tec Deportes</a>
     </div>
     <div class="navbar-collapse collapse navbar-responsive-collapse">
-      
+
       <ul class="nav navbar-nav navbar-right">
         <li><a href="cerrarSesion.php">Cerrar sesión</a></li>
       </ul>
@@ -62,17 +62,17 @@ $sql="select
       <a href="pantallaRegistrarCurso.php?idAlumno=<?php echo $idAlumno;?>&bloque=2"><button class='btn btn-primary btn-xs' >Bloque 2</button></a>
       <a href="pantallaRegistrarCurso.php?idAlumno=<?php echo $idAlumno;?>&bloque=3"><button class='btn btn-primary btn-xs' >Bloque 3</button></a>
       <a href="pantallaRegistrarCurso.php?idAlumno=<?php echo $idAlumno;?>&bloque=4"><button class='btn btn-primary btn-xs' >Bloque 4</button></a>
-     </div>
-	<table class="table table-striped table-hover ">
+    </div>
+    <table class="table table-striped table-hover ">
       <thead>
         <tr>
           <th>#</th>
           <th>Curso</th>
           <th>Cupo</th>
-		      <th>Cupo Actual</th>
+          <th>Cupo Actual</th>
           <th>Num. Maestros</th>
-		      <th>Edad M&iacute;nima</th>
-		      <th>D&iacute;as de la semana</th>
+          <th>Edad M&iacute;nima</th>
+          <th>D&iacute;as de la semana</th>
           <th>Precio</th>
           <th>Inscribir a curso</th>
         </tr>
@@ -80,49 +80,49 @@ $sql="select
       <tbody>
 
         <?php
-  
-    while($row = mysql_fetch_array($result)){
-      $idCurso = $row['idCurso'];
-      $Nombre = $row['Nombre'];
-      $Cupo = $row['Cupo'];
-      $AlumnosInscritos = $row['AlumnosInscritos']; //Restar Cupo - AlumnosInscritos nos dara el cupo actual
-      $CupoActual = $Cupo - $AlumnosInscritos;
-      $NumMaestros = $row['CanMaestros'];
-      $EdadMinima = $row['EdadMinima'];
-      $DiasDeLaSemana = $row['DiasDeLaSemana'];
-      $Precio = $row['Precio'];
 
-      echo "  <tr>
-            <td>$idCurso</td>
-            <td>$Nombre</td>
-            <td>$Cupo</td>
-            <td>$CupoActual</td>
-            <td>$NumMaestros</td>
-            <td>$EdadMinima</td>
-            <td>$DiasDeLaSemana</td>
-            <td>$Precio</td>
-            <form action='inscripcionAlumno.php' method='POST'> 
-              <input type='hidden' name='idCurso' value='$idCurso'/>
-              <input type='hidden' name='idAlumno' value='$idAlumno'/>
-              <td><input type='submit' class='btn btn-primary btn-xs' value='Inscribir a curso'></td>
-            </form>
+        while($row = mysql_fetch_array($result)){
+          $idCurso = $row['idCurso'];
+          $Nombre = $row['Nombre'];
+          $Cupo = $row['Cupo'];
+          $AlumnosInscritos = $row['AlumnosInscritos']; //Restar Cupo - AlumnosInscritos nos dara el cupo actual
+          $CupoActual = $Cupo - $AlumnosInscritos;
+          $NumMaestros = $row['CanMaestros'];
+          $EdadMinima = $row['EdadMinima'];
+          $DiasDeLaSemana = $row['DiasDeLaSemana'];
+          $Precio = $row['Precio'];
+
+          echo "  <tr>
+          <td>$idCurso</td>
+          <td>$Nombre</td>
+          <td>$Cupo</td>
+          <td>$CupoActual</td>
+          <td>$NumMaestros</td>
+          <td>$EdadMinima</td>
+          <td>$DiasDeLaSemana</td>
+          <td>$Precio</td>
+          <form action='inscripcionAlumno.php' method='POST'>
+          <input type='hidden' name='idCurso' value='$idCurso'/>
+          <input type='hidden' name='idAlumno' value='$idAlumno'/>
+          <td><input type='submit' class='btn btn-primary btn-xs' value='Inscribir a curso'></td>
+          </form>
           </tr>";
-    } 
-  ?>
-
-      
-    </tbody>
-  </table>
+        }
+        ?>
 
 
-      <hr>
-      <footer>
-        <p>&copy; ITESM 2014</p>
-      </footer>
-    </div>
+      </tbody>
+    </table>
 
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/bootswatch.js"></script>
-  </body>
-  </html>
+
+    <hr>
+    <footer>
+      <p>&copy; ITESM 2014</p>
+    </footer>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/bootswatch.js"></script>
+</body>
+</html>
