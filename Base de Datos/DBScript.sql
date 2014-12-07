93ffedd1f35e28d391ad4df2b18375e0afe8324b
azaelalanis <azaelalanis.g@gmail.com>
2014-12-07 14:50:00 -0600

    DBScript final

--- Base de Datos/DBScript.sql
+++ Base de Datos/DBScript.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Nov 30, 2014 at 08:53 PM
+-- Generation Time: Dec 07, 2014 at 09:48 PM
 -- Server version: 5.5.38
 -- PHP Version: 5.5.14
 
@@ -38,14 +38,7 @@ CREATE TABLE `alumno` (
   `PapeleriaCompleta` tinyint(1) NOT NULL,
   `ExperienciaAlumno` int(11) NOT NULL,
   `Comentarios` text NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `alumno`
---
-
-INSERT INTO `alumno` (`IdAlumno`, `Nombre`, `FechaNacimiento`, `CURP`, `SeguroMedico`, `NombrePadre`, `Nomina`, `DepartamentoEmpleado`, `ExtensionEmpleado`, `TipoDeContrato`, `Telefono`, `email`, `PapeleriaCompleta`, `ExperienciaAlumno`, `Comentarios`) VALUES
-(1, 'Azael', '2014-11-04', 'CURP', 'Seguro', 'Padre', '', '', '', '', '83174205', 'azael.minuet@hotmail.com', 1, 1, 'Comentarios');
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
@@ -66,14 +59,7 @@ CREATE TABLE `curso` (
   `DiasDeLaSemana` varchar(30) NOT NULL,
   `CanMaestros` int(11) NOT NULL,
   `Precio` int(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `curso`
---
-
-INSERT INTO `curso` (`IdCurso`, `bloque`, `Nombre`, `Cupo`, `EdadMinima`, `EdadMaxima`, `AlumnosInscritos`, `HoraInicio`, `Duracion`, `DiasDeLaSemana`, `CanMaestros`, `Precio`) VALUES
-(1, 0, 'Prueba', 7, 0, 0, 3, '0:0', 60, ' lun,mar', 3, 500);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
@@ -86,8 +72,10 @@ CREATE TABLE `inscripcion` (
   `IdAlumno` int(11) NOT NULL,
   `IdCurso` int(11) NOT NULL,
   `NominaUsuario` varchar(15) NOT NULL,
-  `FormaPago` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `FormaPago` tinyint(1) NOT NULL,
+  `fechaDeInscripcion` datetime DEFAULT NULL,
+  `pagada` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 -- --------------------------------------------------------
 
@@ -107,9 +95,7 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`Nomina`, `Nombre`, `Password`, `Tipo`) VALUES
-('A', 'A', 'A', 0),
-('A01175470', 'Azael', 'ajaxajax', 1),
-('I', 'I', 'I', 1);
+('ADMIN', 'Administracion', 'adminNatacion', 0);
 
 --
 -- Indexes for dumped tables
@@ -147,21 +133,14 @@ ADD PRIMARY KEY (`Nomina`);
 -- AUTO_INCREMENT for table `alumno`
 --
 ALTER TABLE `alumno`
-MODIFY `IdAlumno` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `IdAlumno` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `curso`
 --
 ALTER TABLE `curso`
-MODIFY `IdCurso` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
-
-ALTER TABLE inscripcion
-ADD fechaDeInscripcion DATETIME;
-
-ALTER TABLE inscripcion
-ADD pagada BOOLEAN;
-
+MODIFY `IdCurso` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `inscripcion`
 --
 ALTER TABLE `inscripcion`
-MODIFY `IdInscripcion` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `IdInscripcion` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
